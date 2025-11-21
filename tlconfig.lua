2b513792ab95f8ed9c4a2fb4130e1d888560d7a7
Amrit Bhogal <ambhogal01@gmail.com>
2025-11-21 17:15:32 +0000

    cfgs

--- tlconfig.lua
+++ tlconfig.lua
@@ -1,7 +1,9 @@
 return {
    build_dir = "build",
    source_dir = "src",
-   include_dir = { "src",  "types/extern/types/luajit", "types/", "types/extern/types/penlight", },
+   include_dir = { "src",  "types/extern/types/luajit", "types/", "types/extern/types/penlight", "types/extern/types/luafilesystem", },
    gen_compat = "required",
-   gen_target = "5.1"
+   gen_target = "5.1",
+   -- Skip pruning entirely; build/ contains vendored binaries (stringzilla) we want to keep.
+   dont_prune = { "**" },
 }
