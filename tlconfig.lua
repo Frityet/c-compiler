28a6b10965f3b2086be21c5c150e18dc2312b191
Amrit Bhogal <ambhogal01@gmail.com>
2025-11-23 13:44:10 +0000

    switch to "strict" c99 and use argparse

--- tlconfig.lua
+++ tlconfig.lua
@@ -1,7 +1,7 @@
 return {
    build_dir = "build",
    source_dir = "src",
-   include_dir = { "src",  "types/extern/types/luajit", "types/", "types/extern/types/penlight", "types/extern/types/luafilesystem", },
+   include_dir = { "src",  "types/extern/types/luajit", "types/", "types/extern/types/penlight", "types/extern/types/luafilesystem", "types/extern/types/argparse", },
    gen_compat = "required",
    gen_target = "5.1",
    -- Skip pruning entirely; build/ contains vendored binaries (stringzilla) we want to keep.
